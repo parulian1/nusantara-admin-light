50040e98e4445cb6c9ff536b330386ab00d510f4
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2020-08-19 15:45:07 +0700

    add transformToFormData method in AbstractDetail class to transform form instance to FormData

--- src/app/core/components/abstract-detail.component.ts
+++ src/app/core/components/abstract-detail.component.ts
@@ -107,8 +107,8 @@ export abstract class AbstractDetailComponent<T> extends AbstractEditingComponen
     }
 
     this.form.disable();
-
-    const formData = new FormData(this.formView.nativeElement);
+    // const formData = new FormData(this.formView.nativeElement);
+    const formData = this.transformToFormData(this.form.value);
 
     this.service.save(formData).subscribe(
       resp => {
@@ -118,6 +118,24 @@ export abstract class AbstractDetailComponent<T> extends AbstractEditingComponen
           this.onSaveError(resp);
         }
     });
+  }
+
+  /**
+   * Transform form (FormGroup) value to FormData instance
+   */
+  transformToFormData(result: any): FormData {
+    const formData = new FormData();
+    Object.keys(result).forEach(key => {
+      if (result[key]) {
+        if (Array.isArray(result[key]) && result[key].length === 0) {
+          // ignore it
+          // todo: handling like this because `sourceMappings`, ([]) cannot handling properly
+        } else {
+          formData.append(key, result[key]);
+        }
+      }
+    });
+    return formData;
   }
 
   /**
