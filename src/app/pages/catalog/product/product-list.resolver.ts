18d1289457c3e3f126d83eb519a8f22cc2124774
Heri <duakilo.net@gmail.com>
2020-12-15 17:19:31 +0700

    feat: Add stock listing in product detail

--- src/app/pages/catalog/product/product-list.resolver.ts
+++ src/app/pages/catalog/product/product-list.resolver.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { ProductService } from '@nusantara/services';
 import { PagedResponse } from '@nusantara/core/pagination';
 import { products } from '@nusantara/models';
+import {HttpParams} from '@angular/common/http';
 
 /**
  * Resolves a paginated list of products.
@@ -19,8 +20,25 @@ export class ProductListResolver implements Resolve<PagedResponse<products.IProd
   constructor(private service: ProductService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<PagedResponse<products.IProduct>> | Observable<never> {
-    const query = route.queryParamMap.get('q');
-    const page = parseInt(route.queryParamMap.get('page') || '1', 10);
-    return this.service.fetchParentList(query, page);
+    // const query = route.queryParamMap.get('q');
+    // const page = parseInt(route.queryParamMap.get('page') || '1', 10);
+    // return this.service.fetchParentList(query, page);
+    let params = new HttpParams();
+    const theQuery = route.queryParams;
+    for (const keyParam of Object.keys(theQuery)) {
+      if (['q', 'page', 'per_page', 'include_deleted', ].indexOf(keyParam) >= 0) {
+        if ('page' === keyParam || keyParam === 'per_page') {
+          // need to validate number
+          if (Number.isInteger(theQuery[keyParam])) {
+            // TODO: probably need to throw error
+            continue;
+          }
+        }
+
+        params = params.set(keyParam, theQuery[keyParam]);
+      }
+    }
+
+    return this.service.fetchParams(params);
   }
 }
