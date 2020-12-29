de283aaf87f1783ffe78cbb2d98398ab8c494b34
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2020-12-29 16:26:51 +0700

    refactoring product class, create component for focus handle attributes

---
+++ src/app/pages/catalog/product-class/components/product-class-attributes.component.ts
@@ -0,0 +1,103 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { drf, products } from '@nusantara/models';
+import { IProductAttribute } from '@nusantara/models/products';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'nus-product-class-attributes',
+  template: `
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Searchable</th>
+            <th>Filterable</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody *ngIf="form.value?.length > 0">
+          <tr
+            *ngFor="let attribute of form.controls; let i = index"
+            [formGroup]="attribute"
+          >
+            <td class="immediate-error-display">
+              <input type="text" formControlName="name" maxlength="50" />
+            </td>
+            <td class="immediate-error-display">
+              <select formControlName="type">
+                <option *ngFor="let opt of choices" [ngValue]="opt.value">
+                  {{ opt.displayName }}
+                </option>
+              </select>
+            </td>
+            <td>
+              <input type="checkbox" formControlName="isSearchable" />
+            </td>
+            <td>
+              <input type="checkbox" formControlName="isFilterable" />
+            </td>
+            <td>
+              <button
+                (click)="removeAttribute(i)"
+                type="button"
+                class="remove-button"
+              >
+                <i class="material-icons">remove_circle_outline</i>
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td colspan="5">
+              <button type="button" (click)="addAttribute()" class="add-button">
+                Add Attribute
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `,
+  styles: [``],
+})
+export class ProductClassAttributesComponent implements OnInit {
+  @Input() attributes: IProductAttribute[] = [];
+  @Input() choices: drf.IChoice[] = [];
+  @Input() form: FormArray;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.initialFormValue();
+  }
+
+  initialFormValue(): void {
+    this.attributes.forEach((attribute) => {
+      this.addAttribute(attribute);
+    });
+  }
+
+  removeAttribute(index: number) {
+    this.form.removeAt(index);
+  }
+
+  addAttribute(attr?: products.IProductAttribute) {
+    const attrGroup = this.fb.group({
+      name: [attr?.name, [Validators.required, Validators.maxLength(50)]],
+      href: [attr?.href],
+      type: [attr?.type, [Validators.required]],
+      minValue: [attr?.minValue],
+      maxValue: [attr?.maxValue],
+      isSearchable: [attr?.isSearchable ?? false, []],
+      isFilterable: [attr?.isFilterable ?? false, []],
+    });
+
+    // if the attr already has an href (it exists in the database)
+    // then the name and type may not be changed.
+    if (!!attrGroup.get('href').value) {
+      attrGroup.get('type').disable();
+    }
+
+    this.form.push(attrGroup);
+  }
+}
