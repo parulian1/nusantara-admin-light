871e142bf48ad59d7ee491260e749c58b81c5639
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2021-01-21 12:05:21 +0700

    add warehouses and is_active fields for form in employee section

---
+++ src/app/pages/users/employee/warehouse/employee-warehouse-host.component.ts
@@ -0,0 +1,139 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+import { forkJoin, Observable, zip } from 'rxjs';
+
+import { environment } from '@env/environment';
+import { AuthService } from '@nusantara/auth';
+import { WarehouseService } from '@nusantara/services';
+import { IEmployee, IWarehouse } from '@nusantara/models';
+import { getSlugFromHref } from '@nusantara/shared/helpers';
+
+@Component({
+  selector: 'nus-employee-warehouse-host',
+  template: `
+    <h2>Employee</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Warehouse</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <nus-employee-warehouse-list
+          *ngFor="let formControl of form.controls; let i = index"
+          [choices]="choices"
+          [form]="formControl"
+          (removed)="onRemove($event, i)"
+        >
+        </nus-employee-warehouse-list>
+        <tr>
+          <td colspan="2">
+            <a (click)="addToForm()" style="cursor: pointer;">Add new Warehouse</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  `,
+})
+export class EmployeeWarehouseHostComponent implements OnInit {
+  @Input() entity?: IEmployee;
+  @Input() form: FormArray;
+  @Input() choices: IWarehouse[] = [];
+
+  currentUser: { user_id?: string; href?: string }; // user_id is username
+  deletedWarehouse: IWarehouse[] = [];
+
+  get token(): any {
+    return this.authService.token;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private warehouseService: WarehouseService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.initialFormArray();
+    this.handleCurrentUser();
+  }
+
+  handleCurrentUser(): void {
+    this.currentUser = this.parseJwt(this.authService.token);
+    this.currentUser = {
+      ...this.currentUser,
+      href: `${environment.apiBaseUrl}/api/iam/user/${this.currentUser.user_id}/`,
+    };
+  }
+
+  initialFormArray(): void {
+    this.warehouseService
+      .fetchAllByUser(this.entity?.username)
+      .subscribe((warehouses) => {
+        warehouses.forEach((warehouse) => {
+          this.addToForm(warehouse);
+        });
+      });
+  }
+
+  addToForm(warehouse?: IWarehouse): void {
+    const form = this.fb.group({
+      href: [warehouse?.href ?? ''],
+      name: [warehouse?.name ?? ''],
+    });
+
+    this.form.push(form);
+  }
+
+  onRemove(form: FormGroup, index: number): void {
+    if (!!form.get('href').value) {
+      this.deletedWarehouse.push(form.value as IWarehouse);
+    }
+    this.form.removeAt(index);
+  }
+
+  saveAll(): Observable<any> {
+    const savedJoin$ = forkJoin(
+      this.form.value.map((warehouse) => {
+        return this.warehouseService.createEmployee(
+          getSlugFromHref(warehouse.href),
+          {
+            user: this.currentUser.href,
+          }
+        );
+      }) || []
+    );
+    const deletedJoin$ = forkJoin(
+      this.deletedWarehouse.map((warehouse) => {
+        return this.warehouseService.deleteEmployee(
+          getSlugFromHref(warehouse.href),
+          this.currentUser.user_id
+        );
+      })
+    );
+
+    return zip(savedJoin$, deletedJoin$);
+  }
+
+  /**
+   * Parse the JWT,
+   * so that we can get detail of user
+   * especially user_id aka. username
+   */
+  parseJwt(token): any {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map((c) => {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
+
+    return JSON.parse(jsonPayload);
+  }
+}
