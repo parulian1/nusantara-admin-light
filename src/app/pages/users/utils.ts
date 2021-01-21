7d118dd6994e922bb40adb93ec49321501b422ad
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2021-01-21 15:17:34 +0700

    add send reset password and refactoring

---
+++ src/app/pages/users/utils.ts
@@ -0,0 +1,19 @@
+/**
+ * Parse the JWT,
+ * so that we can get detail of user
+ * especially user_id aka. username
+ */
+export const parseJwt = (token): any => {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+};
