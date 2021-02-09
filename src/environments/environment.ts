04ccbcce01bf9c670dc92ae3d2ed86b325b95cf5
R Farid Nugraha <farid@gramedia.digital>
2021-02-09 12:56:36 +0700

    update pipeline build release nusantars-admin change baseurl enviroments #90875

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   // apiBaseUrl: 'https://staging.bhisma.cloud',
 
-  apiBaseUrl: 'https://dev-marketplace.bhisma.cloud',
+  apiBaseUrl: '/api',
   googleApiKey: 'AIzaSyB7m0r7paaV5I5U6vjf0pDmocvD8-K-D-w',
   appVersion: version,
   elasticAPM: {
