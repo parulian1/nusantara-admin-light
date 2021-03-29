06413da8c8b6604100910948a63338f3d098d52a
Agung Yuliyanto <agung.yuliyanto@gramedia.digital>
2021-03-29 14:05:25 +0700

    [#98976] Basic Price: show only default type, hide add price list

--- src/app/pages/catalog/product/price/price-list.component.ts
+++ src/app/pages/catalog/product/price/price-list.component.ts
@@ -109,7 +109,7 @@ export class PriceListComponent extends AbstractEditingComponent implements OnIn
   private static readonly MINIMUM_QUANTITY  = 1;
   private static readonly DEFAULT_PRICE = 10_000;
 
-  isExpanded = false;
+  isExpanded = true;
 
   @Input() form: FormGroup;
   @ViewChildren(RangeComponent) rangeComponents: QueryList<RangeComponent>;
@@ -136,8 +136,10 @@ export class PriceListComponent extends AbstractEditingComponent implements OnIn
 
   ngOnInit() {
     this.route.data.subscribe((data: {priceListTypes: drf.IChoice[]}) => {
-      this.types = data.priceListTypes;
+      // #97699 and #97726 make type choice only 'default'
+      this.types = data.priceListTypes.filter(priceList => priceList.value === 'default');
     });
+
     // triggers change
     if (!this.enterpriseLicense()) {
       this.toggleExpansion();
@@ -156,7 +158,8 @@ export class PriceListComponent extends AbstractEditingComponent implements OnIn
   }
 
   toggleExpansion() {
-    this.isExpanded = !this.isExpanded;
+    this.isExpanded = true;
+    // this.isExpanded = !this.isExpanded;
   }
 
   /**
