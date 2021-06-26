59f4141da0e8a39b65dedf7faf1b30de96228fa8
Heri Priyatno <heri.priyatno@gramedia.digital>
2021-06-26 23:54:12 +0700

    fix: Test fix - wip

--- src/tests/shared/csv-dialog.component.spec.ts
+++ src/tests/shared/csv-dialog.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CsvDialogComponent } from './csv-dialog.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
+import {SharedModule} from '@nusantara/shared';
+import {CsvDialogComponent} from '@nusantara/shared/csv-dialog/csv-dialog.component';
 
 describe('CsvDialogComponent', () => {
   let component: CsvDialogComponent;
@@ -8,6 +12,12 @@ describe('CsvDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        SharedModule,
+      ],
       declarations: [ CsvDialogComponent ]
     })
     .compileComponents();
