06214e34cf565909c0ab5b39670d413179421fe6
Heri Priyatno <heri.priyatno@gramedia.digital>
2021-07-30 15:33:28 +0700

    Fix: Junit test reporter

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-spec-reporter'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -28,11 +29,22 @@ module.exports = function (config) {
       reporters: [
         // reporters not supporting the `file` property
         { type: 'html', subdir: 'report-html' },
-        { type: 'cobertura', subdir: '.', file: 'TEST-nusantara-admin.xml' },
+        { type: 'cobertura', subdir: '.', file: 'report-cobertura.xml' },
         { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
       ]
     },
-    reporters: ['progress', 'spec', 'kjhtml' , 'coverage'],
+    // the default configuration
+    junitReporter: {
+      outputDir:  require('path').join(__dirname, './coverage/nusantara-admin'), // results will be saved as $outputDir/$browserName.xml
+    //   outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+    //   suite: '', // suite will become the package name attribute in xml testsuite element
+    //   useBrowserName: true, // add browser name to report and classes names
+    //   nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+    //   classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+    //   properties: {}, // key value pair of properties to add to the <properties> section of the report
+    //   xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+    },
+    reporters: ['progress', 'spec', 'junit' , 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
