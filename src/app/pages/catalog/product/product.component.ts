9ab18340f595b4c169ac03f6fb0b9f0815b9e4f1
martogi.sitanggang <martogi@apps-foundry.com>
2021-10-01 14:16:18 +0700

    fix add or remove product inside of bundling causing problem

--- src/app/pages/catalog/product/product.component.ts
+++ src/app/pages/catalog/product/product.component.ts
@@ -1336,17 +1336,25 @@ export class ProductComponent extends AbstractDetailComponent<products.IProduct>
   }
 
   private setPriceProductBundling(): void {
-    this.priceListHost?.updatePriceList({
-      href: null,
-      product: null,
-      type: 'default',
-      platforms: [],
-      locations: [],
-      isProgressive: false,
-      ranges: [
-        {href: null, priceList: null, price: this.totalPrice, minQuantity: 1, maxQuantity: null}
-      ]
-    }, 0);
+    if (!this.priceListHost) {
+      this.priceListHost?.updatePriceList({
+        href: null,
+        product: null,
+        type: 'default',
+        platforms: [],
+        locations: [],
+        isProgressive: false,
+        ranges: [
+          {href: null, priceList: null, price: this.totalPrice, minQuantity: 1, maxQuantity: null}
+        ]
+      }, 0);
+    } else {
+      for (const priceList of this.entity?.priceLists ?? []) {
+        priceList.ranges[0].price = this.totalPrice;
+        this.priceListHost.updatePriceList(priceList, 0);
+      }
+    }
+
   }
 
   updateVirtualAmountAndPriceListAndWeight(): void {
