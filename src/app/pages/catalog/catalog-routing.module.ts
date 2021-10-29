4a349ffb751671b3dbe267aa28f5b407d3891ca1
Heri Priyatno <heri.priyatno@gramedia.digital>
2021-10-29 15:42:34 +0700

    fix: Remove product class resolving in product detail routing

--- src/app/pages/catalog/catalog-routing.module.ts
+++ src/app/pages/catalog/catalog-routing.module.ts
@@ -90,7 +90,7 @@ const routes: Routes = [
         component: ProductComponent,
         resolve: {
           entity: ProductResolver,
-          productClasses: AllProductClassResolver,
+          // productClasses: AllProductClassResolver,
           // vendors: AllVendorResolver,
           // categories: AllCategoryResolver,
           mediaTypes: MediaTypeResolver,
