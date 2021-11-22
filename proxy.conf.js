3ea3b590f105e069f79692f95c2ed69c7f4be687
martogi.sitanggang <martogi@apps-foundry.com>
2021-11-22 13:28:57 +0700

    refactor code and fix unit test

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,13 +1,13 @@
 
 const proxyConfig = [
 
-  // {
-  //   context: '/api',
-  //   // pathRewrite: {'^/api/iam': ''},
-  //   target: 'http://127.0.0.1:4201',
-  //   changeOrigin: true,
-  //   secure: false
-  // },
+  {
+    context: '/api',
+    // pathRewrite: {'^/api/iam': ''},
+    target: 'https://dev-ecm-mt.bhisma.cloud/api',
+    changeOrigin: true,
+    secure: false
+  },
 
   // {
   //   context: 'https://bhisma.cloud/api/iam',
@@ -16,20 +16,48 @@ const proxyConfig = [
   //   changeOrigin: true,
   //   secure: false
   // },
-  {
-    context: '/api/catalog',
-    pathRewrite: {'^/api/catalog': ''},
-    target: 'http://127.0.0.1:8001',
-    changeOrigin: true,
-    secure: false
-  },
-  {
-    context: '/api/iam',
-    // pathRewrite: {'^/api/iam': ''},
-    target: 'https://bhisma.cloud/api/iam',
-    changeOrigin: true,
-    secure: false
-  }
+  // {
+  //   context: '/api/catalog',
+  //   pathRewrite: {'^/api/catalog': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/catalog',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
+  // {
+  //   context: '/api/order',
+  //   pathRewrite: {'^/api/order': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/order',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
+  // {
+  //   context: '/api/fulfillment',
+  //   pathRewrite: {'^/api/fulfillment': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/fulfillment',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
+  // {
+  //   context: '/api/cms',
+  //   pathRewrite: {'^/api/cms': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/cms',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
+  // {
+  //   context: '/api/iam',
+  //   pathRewrite: {'^/api/iam': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/iam',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
+  // {
+  //   context: '/api/client',
+  //   pathRewrite: {'^/api/client': ''},
+  //   target: 'https://dev-ecm.bhisma.cloud/api/client',
+  //   changeOrigin: true,
+  //   secure: false
+  // },
 ]
 
 module.exports = proxyConfig;
