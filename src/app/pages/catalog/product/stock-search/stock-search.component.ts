78777d38d3b5b724c3c570139c9405fe8664b44a
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-03-23 16:33:43 +0700

    SkuInfoPDP - Add SKU list on product inventory PDP #171459

--- src/app/pages/catalog/product/stock-search/stock-search.component.ts
+++ src/app/pages/catalog/product/stock-search/stock-search.component.ts
@@ -1,26 +1,48 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { WarehouseService } from '@nusantara/services';
+import { InventoryStockRecordService, WarehouseService } from '@nusantara/services';
 import { IStockSearch } from '@nusantara/models/products/stock-search';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'nus-stock-search',
   template: `
     <h3 i18n>Product Inventory</h3>
-    <table *ngIf="!!entity">
+    <table *ngIf="!!entity" class="table-accordion">
       <thead>
-        <tr>
-          <th i18n>Warehouse</th>
-          <th i18n>Quantity</th>
-        </tr>
+      <tr>
+        <th i18n>Warehouse (Location)</th>
+        <th i18n>Stock</th>
+      </tr>
       </thead>
       <tbody>
       <ng-container *ngFor="let ent of entity">
-        <tr *ngFor="let sublocation of ent.subLocations">
-          <td>
-            {{ ent.name }} ({{ sublocation.name }})
-          </td>
-          <td data-qa="quantity">{{ sublocation.quantity }}</td>
-        </tr>
+        <ng-container *ngFor="let subLocation of ent.subLocations">
+          <tr>
+            <td colspan="2">
+
+              <table class="table-accordion-nested">
+                <tr [id]="'heading'+subLocation.id" class="panel-heading" role="tab"
+                    (click)="fetchSkuList(ent.href, subLocation.href)">
+                  <td>
+                    {{ ent.name }} ({{ subLocation.name | titlecase }})
+                  </td>
+                  <td data-qa="quantity">{{ subLocation.quantity }}</td>
+                </tr>
+                <ng-container *ngIf="subLocation.skuList.length > 0">
+                  <tr class="panel-body" role="tabpanel" [attr.aria-labelledby]="'heading'+subLocation.id"
+                      [@contentExpansion]="subLocation.displaySku ? 'expanded':'collapsed'"
+                      *ngFor="let item of subLocation.skuList">
+                    <td>
+                        {{item.sku}}
+                    </td>
+                    <td>{{item.latestStock}}</td>
+                  </tr>
+                </ng-container>
+              </table>
+
+            </td>
+          </tr>
+        </ng-container>
       </ng-container>
       </tbody>
     </table>
@@ -28,6 +50,58 @@ import { IStockSearch } from '@nusantara/models/products/stock-search';
   `,
   styles: [
     'h3 { font-size: 20px; margin: 0 0 20px 0; }',
+    `
+      .table-accordion > tbody > tr > td {
+        padding: 0;
+      }
+
+      .table-accordion tr th:first-child {
+        width: 85%;
+      }
+
+      .table-accordion-nested {
+        border-collapse: collapse;
+        border: none;
+      }
+
+      .table-accordion-nested tr td {
+        border-collapse: collapse;
+        border: none;
+      }
+
+      .table-accordion-nested tr td:first-child {
+        width: 85%;
+      }
+
+      .panel-heading {
+        font-weight: 700;
+      }
+
+      .panel-heading > td:first-child {
+        color: var(--bhisma-orange);
+      }
+
+      /* Style the accordion panel. Note: hidden by default */
+      /*.collapsed {*/
+      /*  height: 0;*/
+      /*  opacity: 0;*/
+      /*  visibility: hidden;*/
+      /*}*/
+      /*.expanded {*/
+      /*  height: max-content;*/
+      /*  opacity: 1;*/
+      /*  visibility: visible;*/
+      /*}*/
+    `
+  ],
+  animations: [
+    trigger('contentExpansion', [
+      state('expanded', style({height: 'max-content', opacity: 1, visibility: 'visible'})),
+      state('collapsed', style({height: '0', opacity: 0, visibility: 'hidden'})),
+      transition('expanded <=> collapsed', [
+        animate('200ms cubic-bezier(.37,1.04,.68,.98)')
+      ]),
+    ])
   ]
 })
 export class StockSearchComponent implements OnInit {
@@ -37,12 +111,42 @@ export class StockSearchComponent implements OnInit {
 
   public entity: IStockSearch[];
 
-  constructor(public warehouseService: WarehouseService) { }
+  constructor(public warehouseService: WarehouseService, public stockRecordService: InventoryStockRecordService) { }
 
   ngOnInit(): void {
     this.warehouseService.warehouseStockSearchWithDetails(this.productHref).subscribe( res => {
       this.entity = res;
+      for (const warehouse of this.entity) {
+        for (const location of warehouse.subLocations) {
+          // Add empty skuList, later skuList will be fetched everytime user click location row for detail sku list
+          location.skuList = [];
+          location.displaySku = false;
+        }
+      }
     });
   }
 
+  fetchSkuList(warehouseHref, locationHref) {
+    const warehouseObject = this.entity.find(({href}) => href === warehouseHref);
+    const locationObject = warehouseObject.subLocations.find(({href}) => href === locationHref);
+
+    if (!locationObject.displaySku) {
+      this.stockRecordService.fetchListStockRecordSearch(
+        {
+          productHref: this.productHref,
+          sublocationHref: locationHref,
+          receivingOrderStatus: 'approved',
+          groupBy: ['sku']
+        }
+      ).subscribe(res => {
+        if (res.length > 0) {
+          locationObject.skuList = res;
+        }
+      });
+      locationObject.displaySku = true;
+    } else {
+      locationObject.skuList = [];
+      locationObject.displaySku = false;
+    }
+  }
 }
