a11ecf40fae6db94700792cd669f44db22c13982
galuh <galuh.ardiansyah@gramedia.digital>
2022-04-20 13:54:52 +0700

    update fields error

--- src/app/shared/field-errors.component.ts
+++ src/app/shared/field-errors.component.ts
@@ -31,7 +31,7 @@ import { FormControl } from '@angular/forms';
         Ensure this value is less than or equal to {{ control.getError('max')?.max }}
       </div>
       <div *ngIf="control?.errors?.forbiddenName" i18n>
-        Accept only characters (&.!)
+        Accept only characters (&.!\/[]-+_)
       </div>
       <div *ngIf="control?.errors?.invalidStock" i18n>
         Limited stock
