57b5e35aa00774dd26e9352179df108695d9cc66
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-05-09 21:03:21 +0700

    LowStockProduct - Add product upc info and filter by upc #190300

--- src/app/pages/config/low-stock-config/low-stock-product-list.component.ts
+++ src/app/pages/config/low-stock-config/low-stock-product-list.component.ts
@@ -15,7 +15,7 @@ import {MatSelectChange} from '@angular/material/select';
       <div class="list-page-header">
         <div class="search control">
           <i class="material-icons" i18n>search</i>
-          <input type="search" id="search_box" [formControl]="searchText" placeholder="Search Product Name or SKU">
+          <input type="search" id="search_box" [formControl]="searchText" placeholder="Search Product Name dan UPC">
         </div>
         <div class="filter">
           <mat-form-field>
@@ -46,6 +46,7 @@ import {MatSelectChange} from '@angular/material/select';
         <thead>
         <tr>
           <th i18n>Name</th>
+          <th i18n>UPC</th>
           <th i18n>Warehouse</th>
           <th i18n>Location</th>
           <th i18n>Qty</th>
@@ -54,6 +55,7 @@ import {MatSelectChange} from '@angular/material/select';
         <tbody *ngIf="displayedResults?.totalResults > 0">
         <tr *ngFor="let entity of displayedResults.entities">
           <td>{{entity.name}}</td>
+          <td>{{entity.upc}}</td>
           <td>{{entity.warehouseName}}</td>
           <td>{{entity.sublocationName}} ({{entity.sublocationType}})</td>
           <td>{{entity.latestStock}}</td>
