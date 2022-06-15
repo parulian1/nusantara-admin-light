8c3de10cdb588d0c70b2b1b650ed5b67e7026ca9
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-06-15 13:49:08 +0700

    PageTitle - Update promo list page title

--- src/app/pages/promotion/promotion/single/promotion-single-list.component.ts
+++ src/app/pages/promotion/promotion/single/promotion-single-list.component.ts
@@ -7,7 +7,7 @@ import { IProductPromotion } from '@nusantara/models';
 @Component({
   selector: 'nus-promotion-single-list',
   template: `
-    <h1 class="title-1">Promotion</h1>
+    <h1 class="title-1" i18n>Promos</h1>
     <div class="tab">
       <div (click)="goToPromoCampaign()">
         <strong>Campaign</strong>
