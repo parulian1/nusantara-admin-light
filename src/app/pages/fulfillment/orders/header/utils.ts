c4d2de41df6f53f384be65fe4d48f55e83a7496e
galuh <galuh.ardiansyah@gramedia.digital>
2022-06-20 18:41:03 +0700

    remove unnecessary code

--- src/app/pages/fulfillment/orders/header/utils.ts
+++ src/app/pages/fulfillment/orders/header/utils.ts
@@ -5,19 +5,12 @@ const apiDateFormat = "YYYY-MM-DDTHH:mm:ss";
 export class Utils {
   get today() { return this.setTimeToZero(moment()); }
   get yesterday() { return this.setTimeToZero(moment(moment().subtract(1, "days"))); }
-  get yesterdayend() { return this.setTimeEndDay(moment(moment().subtract(1, "days"))); }
   get threeDaysbefore() { return this.setTimeToZero(moment(moment().subtract(3, "days"))); }
   get sevenDaysbefore() { return this.setTimeToZero(moment(moment().subtract(7, "days"))); }
 
   setTimeToZero(date: moment.Moment): string {
     return date
       .set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
-      .format(apiDateFormat);
-  }
-
-  setTimeEndDay(date: moment.Moment): string {
-    return date
-      .set({ hour: 23, minute: 59, second: 59, millisecond: 999 })
       .format(apiDateFormat);
   }
 
@@ -36,7 +29,6 @@ export class Utils {
       } else if (startTime === this.yesterday && endTime === this.yesterday && !isCustomDate && !isDateRange){
         return "yesterday";
       } else if (moment(endTime).diff(moment(startTime), "days") === 0 && !isDateRange) {
-        console.log(!isDateRange)
         return "customDate";
       } else {
         return "customRange";
