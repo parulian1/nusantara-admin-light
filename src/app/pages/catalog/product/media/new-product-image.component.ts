d124f4374f49e2d54040827edcda85751f745f8f
Heri Priyatno <heri.priyatno@gramedia.digital>
2022-06-21 14:30:45 +0700

    feat: Product multi image upload
    #213532 Allow multiple image upload in product detail admin
    Add limit to allowed file image size in product media
    Add multiple file support for file size validation
    Add limit to number of uploaded image
    
    update xlsx package to latest

--- src/app/pages/catalog/product/media/new-product-image.component.ts
+++ src/app/pages/catalog/product/media/new-product-image.component.ts
@@ -1,9 +1,11 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
-import { AbstractEditingComponent, DialogResult } from '@nusantara/core';
-import { NgxSmartModalComponent } from 'ngx-smart-modal';
-import {fileTypeValidator} from '@nusantara/core/helpers/validators';
+import {AbstractEditingComponent, DialogResult, Logger} from '@nusantara/core';
+import {NgxSmartModalComponent} from 'ngx-smart-modal';
+import {fileSizeValidator, fileTypeValidator, maxFileValidator} from '@nusantara/core/helpers/validators';
+
+const logger = new Logger('NewProductImage');
 
 /**
  * Dialog component that allows the user to select a single image
@@ -16,8 +18,13 @@ import {fileTypeValidator} from '@nusantara/core/helpers/validators';
   template: `
     <ngx-smart-modal [identifier]="'newImageModal'" #modal [formGroup]="form" [title]="'Upload Image'" *ngIf="!!form">
       <h1 i18n>Upload Image</h1>
+      <p i18n>Maximum of {{(maxNumber - priorityValue) + 1}} file allowed, with {{MAX_FILE_SIZE}} KB file size</p>
       <form #modalForm>
-        <img [src]="imagePreviewUrl" alt="Image Preview">
+        <ng-container *ngIf="!!imagePreviewUrls && imagePreviewUrls?.length > 0">
+          <div class="image-preview">
+            <img *ngFor="let imageUrl of imagePreviewUrls" [src]="imageUrl" alt="Image Preview" i18n-alt>
+          </div>
+        </ng-container>
         <input type="hidden" [formControl]="href" name="href">
         <input type="hidden" [formControl]="type" name="type">
         <input type="hidden" [formControl]="youtubeVideoId" name="youtubeVideoId">
@@ -25,7 +32,9 @@ import {fileTypeValidator} from '@nusantara/core/helpers/validators';
         <input type="file" [formControl]="image"
                (change)="setMediaImage($event)"
                accept="image/jpeg, image/png"
+               multiple
                #imageInput name="image">
+        <nus-field-errors [control]="image"></nus-field-errors>
         <input type="hidden" [formControl]="sortPriority" name="sortPriority">
         <input type="hidden" [formControl]="identifier" name="identifier">
         <button [disabled]="form.invalid" (click)="close()" type="button" class="control" i18n>Save</button>
@@ -33,9 +42,21 @@ import {fileTypeValidator} from '@nusantara/core/helpers/validators';
       </form>
     </ngx-smart-modal>
   `,
-  styles: [ 'img { max-width: 100%; }' ]
+  styles: ['img { max-width: 100%; }',
+    `.image-preview {
+      display: flex;
+      max-width: 90%;
+      flex-wrap: wrap;
+      gap: 8px 8px;
+    }
+
+    .image-preview img {
+      max-height: 48px;
+    }
+    `]
 })
 export class NewProductImageComponent extends AbstractEditingComponent implements OnInit, AfterViewInit {
+  readonly MAX_FILE_SIZE = 2048;
 
   @ViewChild('imageInput') imageInput: ElementRef;
   @ViewChild('modalForm') formView: ElementRef<HTMLFormElement>;
@@ -45,6 +66,13 @@ export class NewProductImageComponent extends AbstractEditingComponent implement
 
   imagePreviewUrl: string;
   priorityValue = 1;
+
+  imagePreviewUrls: Array<string> = [];
+  imageUploadForm: Array<{
+    fd: FormData,
+    fg: FormGroup,
+  }>;
+  public maxNumber: number;
 
   constructor(protected fb: FormBuilder) {
     super();
@@ -68,32 +96,53 @@ export class NewProductImageComponent extends AbstractEditingComponent implement
   private initializeForm(): void {
     this.form = this.fb.group({
       href: ['', []],
-      image: ['', [Validators.required, ]],
-      sortPriority: [this.priorityValue, [Validators.required, ]],
-      identifier: [  this.randomString(10), [Validators.required, ]],
-      type: ['image', [Validators.required, ]],
+      image: ['', [Validators.required,]],
+      sortPriority: [this.priorityValue, [Validators.required,]],
+      identifier: [this.randomString(10), [Validators.required,]],
+      type: ['image', [Validators.required,]],
       youtubeVideoId: [null, []],
       imageName: ['', []]
     });
+    this.imagePreviewUrls = [];
+    this.imageUploadForm = [];
   }
 
-  get type(): FormControl { return this.form.get('type') as FormControl; }
-  get image(): FormControl { return this.form.get('image') as FormControl; }
-  get youtubeVideoId(): FormControl { return this.form.get('youtubeVideoId') as FormControl; }
-  get sortPriority(): FormControl { return this.form.get('sortPriority') as FormControl; }
-  get identifier(): FormControl { return this.form.get('identifier') as FormControl; }
+  get type(): FormControl {
+    return this.form.get('type') as FormControl;
+  }
 
-  setMediaImage(data?: Event|string) {
+  get image(): FormControl {
+    return this.form.get('image') as FormControl;
+  }
+
+  get youtubeVideoId(): FormControl {
+    return this.form.get('youtubeVideoId') as FormControl;
+  }
+
+  get sortPriority(): FormControl {
+    return this.form.get('sortPriority') as FormControl;
+  }
+
+  get identifier(): FormControl {
+    return this.form.get('identifier') as FormControl;
+  }
+
+  setMediaImage(data?: Event | string) {
     if (data instanceof Event) {
+      logger.debug('setMediaImage', data);
+      this.imagePreviewUrls = [];
+      this.imageUploadForm = [];
       this.image.setValidators([
         Validators.required,
+        maxFileValidator((this.maxNumber - this.priorityValue) + 1, (data?.target as HTMLInputElement)?.files),
         fileTypeValidator(['image/jpg', 'image/jpeg', 'image/png'],
-          (data?.target as HTMLInputElement)?.files )
+          (data?.target as HTMLInputElement)?.files),
+        fileSizeValidator(this.MAX_FILE_SIZE, (data?.target as HTMLInputElement)?.files),
       ]);
       this.image.updateValueAndValidity();
       this.form.get('imageName').patchValue((data?.target as HTMLInputElement)?.files[0].name);
     }
-    this.setImagePreview(data,  (url) => this.imagePreviewUrl = url);
+    this.setImagePreview(data, (url) => this.imagePreviewUrl = url);
   }
 
   getValue(): FormData {
@@ -103,8 +152,9 @@ export class NewProductImageComponent extends AbstractEditingComponent implement
     return new FormData(this.formView.nativeElement);
   }
 
-  open(priority?: number) {
+  open(priority?: number, maxNumber?: number) {
     this.priorityValue = !!priority ? priority : 1;
+    this.maxNumber = !!maxNumber ? maxNumber : 0;
     this.modal.open();
   }
 
@@ -124,10 +174,80 @@ export class NewProductImageComponent extends AbstractEditingComponent implement
   randomString(length) {
     const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
-    for ( let i = 0; i < length; i++ ) {
+    for (let i = 0; i < length; i++) {
       result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
     }
     return result;
+  }
+
+  setImagePreview(data: Event | string, setterFn: (dataAsUrl) => void) {
+    if (!data) {
+      setterFn(this.emptyImagePreviewURL);
+    } else if (data instanceof Event) {
+      const target = data.target as HTMLInputElement;
+      let xForm = null;
+      let xData: any = null;
+      for (let x = 0; x < target.files.length; x++) {
+        let formData: FormData = null;
+
+        this.readFileURLImage(target.files[x], (url) => {
+          this.imagePreviewUrls[x] = url;
+          const img = new Image();
+          img.src = url;
+          img.onload = () => {
+            logger.debug('image-size', img.width, img.height);
+            if (img.width < 300 || img.height < 300) {
+
+            }
+          };
+        });
+        const ident = this.randomString(10);
+        formData = new FormData();
+        formData.append('href', '');
+        formData.append('sortPriority', (this.priorityValue + x).toString(10));
+        formData.append('type', 'image');
+        formData.append('youtubeVideoId', null);
+        formData.append('identifier', ident);
+        formData.append('imageName', target.files[x].name);
+        formData.append('image', target.files[x]);
+
+        xForm = this.fb.group({
+          href: ['', []],
+          image: ['', [Validators.required,]],
+          sortPriority: [this.priorityValue + x, [Validators.required,]],
+          identifier: [ident, [Validators.required,]],
+          type: ['image', [Validators.required,]],
+          youtubeVideoId: [null, []],
+          imageName: [target.files[x].name, []]
+        });
+        logger.debug(target.files[x]);
+        xForm.get('image').patchValue(target.files[x]);
+
+        xData = {
+          fd: formData,
+          fg: xForm
+        };
+
+        this.imageUploadForm.push(xData);
+      }
+    } else {
+      setterFn(data);
+    }
+  }
+
+  /**
+   * Reads the file set on an HTMLInputElement, and returns that file as a data url
+   * that is returned to the callback function.  This is only safe to call on
+   * image.
+   */
+  readFileURLImage(file: File, callback: (dataAsURL: string) => void) {
+    if (!!file) {
+      const reader = new FileReader();
+      reader.onload = (ev) => callback(reader.result as string);
+      reader.readAsDataURL(file);
+    } else {
+      callback(this.emptyImagePreviewURL);
+    }
   }
 }
 
