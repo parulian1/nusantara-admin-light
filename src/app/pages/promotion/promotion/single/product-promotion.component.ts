ea1b4724135c092c5312a08d78dcf7440537ee86
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-08-04 15:05:38 +0700

    ProductPromotion - Update promo type label options

--- src/app/pages/promotion/promotion/single/product-promotion.component.ts
+++ src/app/pages/promotion/promotion/single/product-promotion.component.ts
@@ -36,7 +36,7 @@ const log = new Logger('ProductPromotionComponent');
       <label>
         <span i18n>Type</span>
         <select [formControl]="type" (ngModelChange)="onPromoTypeChange($event)">
-          <option *ngFor="let t of types" [ngValue]="t">{{ t }}</option>
+          <option *ngFor="let t of types" [ngValue]="t">{{ t | promoTypeToLabel }}</option>
         </select>
       </label>
 
