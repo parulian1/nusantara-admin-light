f14b1b631b9e8e9bc9ac059257183608cf66603e
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-08-10 17:06:52 +0700

    ProductPromotion - Update promo type label for free_gift

--- src/app/core/helpers/promo-type-label.ts
+++ src/app/core/helpers/promo-type-label.ts
@@ -4,7 +4,7 @@ export function getPromoTypeLabel(type_val: string) {
     'amount_off': 'Amount Off',
     'override_price': 'Override Price',
     'promo_bundling': 'Promo Bundling',
-    'free_gift': 'Gift Item',
+    'free_gift': 'Free Gift / Lucky Dip',
     'multiply_point': 'Multiply Loyalty Point',
   }
   return promoTypeChoices[type_val];
