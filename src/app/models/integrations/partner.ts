0cd3fdbfe29684577eda0d0797bf3105ae9b8da0
galuh <galuh.ardiansyah@gramedia.digital>
2022-11-03 14:24:32 +0700

    update partner model

--- src/app/models/integrations/partner.ts
+++ src/app/models/integrations/partner.ts
@@ -1,5 +1,5 @@
 export interface IPartner {
-  name: string;
+  name?: string;
   partner: string;
   slug: string;
   clientId: number;
@@ -8,4 +8,7 @@ export interface IPartner {
   customerId: number;
   access: string;
   isActive: boolean;
+  forstokId:string;
+  secretKey: string;
+  warehouse:string;
 }
