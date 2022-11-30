8e1d45d14625c95e129e2950a153f63c786f1797
Kevin Jonathan <kevin.jonathan@gramedia.digital>
2022-11-30 16:28:23 +0700

    Shipping - Fix shipping provide form layout and fix shipping service validation

--- src/app/pages/config/shipping/shipping-provider-detail.component.ts
+++ src/app/pages/config/shipping/shipping-provider-detail.component.ts
@@ -3,7 +3,7 @@ import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { AbstractDetailComponent, ToastService } from '@nusantara/core';
-import { IShippingProvider, IShippingService, drf, ISocialMedia } from '@nusantara/models';
+import { IShippingProvider, IShippingService, drf } from '@nusantara/models';
 import { ShippingProviderService } from '@nusantara/services';
 import { Kgx, ShippingServicesTypes } from './shipping-service/constants';
 
@@ -19,79 +19,82 @@ import { Kgx, ShippingServicesTypes } from './shipping-service/constants';
 
     <form [formGroup]="form" (ngSubmit)="save()" #f>
       <div class="container">
-        <div class="general-info">
+        <div class="main-content">
+          <div class="wrapper">
+            <h2 class="heading-1" i18n>General Information</h2>
 
-          <label>
-            <span i18n>Name</span>
-            <input type="text" formControlName="name" maxlength="25">
-            <nus-field-errors [control]="name"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Name</span>
+              <input type="text" formControlName="name" maxlength="25">
+              <nus-field-errors [control]="name"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Type</span>
-            <select formControlName="type">
-              <option disabled selected [ngValue]="null"> --Select-- </option>
-              <option *ngFor="let choice of types" [ngValue]="choice.value">{{ choice.displayName }}</option>
-            </select>
-            <nus-field-errors [control]="type"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Type</span>
+              <select formControlName="type">
+                <option disabled selected [ngValue]="null"> --Select-- </option>
+                <option *ngFor="let choice of types" [ngValue]="choice.value">{{ choice.displayName }}</option>
+              </select>
+              <nus-field-errors [control]="type"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Auth User</span>
-            <input type="text" formControlName="authUser">
-            <nus-field-errors [control]="authUser"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Auth User</span>
+              <input type="text" formControlName="authUser">
+              <nus-field-errors [control]="authUser"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Auth Key</span>
-            <input type="text" formControlName="authPass">
-            <nus-field-errors [control]="authPass"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Auth Key</span>
+              <input type="text" formControlName="authPass">
+              <nus-field-errors [control]="authPass"></nus-field-errors>
+            </label>
 
-          <label class="checkbox">
-            <span i18n>Is Active</span>
-            <input type="checkbox" formControlName="isActive">
-          </label>
+            <label class="toggle">
+              <input type="checkbox"
+                     class="toggle"
+                     [formControl]="isActive"
+                     name="is-active"/>
+              <span i18n>Is Active</span>
+              <nus-field-errors [control]="isActive"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Icon</span>
-            <img [src]="iconPreviewUrl" alt="Shipping Method Icon" class="preview">
-            <input type="file" [formControl]="icon" (change)="setIconImagePreview($event)"
-                   name="icon" accept="image/*">
-            <nus-field-errors [control]="icon"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Icon</span>
+              <img [src]="iconPreviewUrl" alt="Shipping Method Icon" class="preview">
+              <input type="file" [formControl]="icon" (change)="setIconImagePreview($event)"
+                     name="icon" accept="image/*">
+              <nus-field-errors [control]="icon"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Description</span>
-            <input type="text" formControlName="description">
-            <nus-field-errors [control]="description"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Description</span>
+              <input type="text" formControlName="description">
+              <nus-field-errors [control]="description"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Sender Name</span>
-            <input type="text" formControlName="senderName">
-            <nus-field-errors [control]="senderName"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Sender Name</span>
+              <input type="text" formControlName="senderName">
+              <nus-field-errors [control]="senderName"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Sender Email</span>
-            <input type="text" formControlName="senderEmail">
-            <nus-field-errors [control]="senderEmail"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Sender Email</span>
+              <input type="text" formControlName="senderEmail">
+              <nus-field-errors [control]="senderEmail"></nus-field-errors>
+            </label>
 
-          <label>
-            <span i18n>Sender Phone</span>
-            <input type="text" formControlName="senderPhone">
-            <nus-field-errors [control]="senderPhone"></nus-field-errors>
-          </label>
+            <label>
+              <span i18n>Sender Phone</span>
+              <input type="text" formControlName="senderPhone">
+              <nus-field-errors [control]="senderPhone"></nus-field-errors>
+            </label>
 
-          <ng-container *ngIf="type.value">
-            <div class="sosmed-title">
-              <h3 i18n>
-                Shipping Service Settings
-              </h3>
-            </div>
-            <hr/>
+          </div>
+          <div class="wrapper" *ngIf="type.value">
+            <h2 class="heading-1" i18n>Shipping Service Settings</h2>
+
             <table class="line-items">
               <thead>
               <tr>
@@ -125,20 +128,25 @@ import { Kgx, ShippingServicesTypes } from './shipping-service/constants';
               </tr>
               </tbody>
             </table>
-          </ng-container>
-          <nus-detail-actions
-            [component]="this"
-            (cancel)="navigateToParent(true)"
-            (delete)="delete()">
-          </nus-detail-actions>
+          </div>
         </div>
       </div>
+      <nus-detail-actions
+        [component]="this"
+        (cancel)="navigateToParent(true)"
+        (delete)="delete()">
+      </nus-detail-actions>
 
     </form>
   `,
   styles: [
+    'h1, h2 { margin-bottom: 0.75rem; }',
     'form{ max-width: none;}',
     '.container { display: grid; grid-template-columns: 4fr 1fr; grid-gap: 24px; }',
+    '.wrapper { border: 1px solid var(--grey); border-radius: 4px; padding: 16px 24px; }',
+    '.wrapper:not(:last-child) { margin-bottom: 24px; }',
+    '.wrapper label { min-height: 0; }',
+    '.wrapper span{ font-weight: 700; color: var(--darken-grey); }',
   ]
 })
 export class ShippingProviderDetailComponent extends AbstractDetailComponent<IShippingProvider> implements OnInit {
@@ -175,6 +183,7 @@ export class ShippingProviderDetailComponent extends AbstractDetailComponent<ISh
   get senderName(): FormControl { return this.form.get('senderName') as FormControl; }
   get senderEmail(): FormControl { return this.form.get('senderEmail') as FormControl; }
   get senderPhone(): FormControl { return this.form.get('senderPhone') as FormControl; }
+  get isActive(): FormControl { return this.form.get('isActive') as FormControl; }
 
   initializeForm(entity?: IShippingProvider) {
     this.form = this.fb.group({
@@ -213,9 +222,9 @@ export class ShippingProviderDetailComponent extends AbstractDetailComponent<ISh
       isActive: [service?.isActive ?? false, []],
       name: [service?.name, [Validators.required]],
       icon: [service?.icon ?? '', []],
-      minimumWeight: [service?.minimumWeight ?? 0, [Validators.required]],
-      handlingFee: [service?.handlingFee ?? 0, [Validators.required]],
-      graceAmount: [service?.graceAmount ?? 0, [Validators.required]],
+      minimumWeight: [service?.minimumWeight ?? 0, [Validators.required, Validators.max(999999999.9999)]],
+      handlingFee: [service?.handlingFee ?? 0, [Validators.required, Validators.max(999999999.9999)]],
+      graceAmount: [service?.graceAmount ?? 0, [Validators.required, Validators.max(9999.99)]],
       description: [service?.description ?? '', []]
     });
 
